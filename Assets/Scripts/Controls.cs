6c11ef5cb4341f257ea94fab98c520d2b0a51f81
FusTaFah <mfadel_1990@hotmail.com>
2017-01-17 20:03:37 +0000

    changes controls

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -6,6 +6,8 @@ public class Controls : MonoBehaviour {
 
     bool m_inPlay;
 
+    public bool InPlay { get { return m_inPlay; } }
+
 	// Use this for initialization
 	void Start () {
         m_inPlay = true;
@@ -13,49 +15,16 @@ public class Controls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (m_inPlay)
-        {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                //move left
-                if(!checkBounds(new Vector3(-1.0f, 0.0f, 0.0f)))
-                {
-                    gameObject.transform.position += new Vector3(-1.0f, 0.0f, 0.0f);
-                }
-                
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                //move right
-                if (!checkBounds(new Vector3(1.0f, 0.0f, 0.0f)))
-                {
-                    gameObject.transform.position += new Vector3(1.0f, 0.0f, 0.0f);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.W))
-            {
-                //move up
-                if (!checkBounds(new Vector3(0.0f, 1.0f, 0.0f)))
-                {
-                    gameObject.transform.position += new Vector3(0.0f, 1.0f, 0.0f);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                //move down
-                if (!checkBounds(new Vector3(0.0f, -1.0f, 0.0f)))
-                {
-                    gameObject.transform.position += new Vector3(0.0f, -1.0f, 0.0f);
-                }
-            }
-        }
+
 	}
 
     public bool checkBounds(Vector3 direction)
     {
         Ray assumedObstacleDirection = new Ray(gameObject.transform.position, direction);
-        if(Physics.Raycast(assumedObstacleDirection, 1.0f))
+        RaycastHit raycastIdentity;
+        if (Physics.Raycast(assumedObstacleDirection, out raycastIdentity, 1.0f))
         {
+            if(raycastIdentity.collider.gameObject.GetComponent<Controls>())
             return true;
         }
         return false;
