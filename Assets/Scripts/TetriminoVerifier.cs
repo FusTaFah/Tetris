247edcc901dbca24b7a1808d6b8a6b6651bef98c
FusTaFah <mfadel_1990@hotmail.com>
2017-01-23 18:45:14 +0000

    adds verifier code

--- Assets/Scripts/TetriminoVerifier.cs
+++ Assets/Scripts/TetriminoVerifier.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TetriminoVerifier : MonoBehaviour {
 
+    Vector3 m_startingPosition;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,6 +18,25 @@ public class TetriminoVerifier : MonoBehaviour {
 
     public void SignalTetriminoVerifier()
     {
-        for(int i = 0; i < )
+        //do a massive amount of computation for no fucking reason
+        for(int i = 0; i < 13; i++)
+        {
+            int lineScore = 0;
+            RaycastHit[] hits = Physics.RaycastAll(new Ray(gameObject.transform.position, new Vector3(1.0f, 0.0f, 0.0f)));
+            foreach(RaycastHit hit in hits)
+            {
+                if(hit.collider.gameObject.tag == "TetriminoPiece")
+                {
+                    lineScore++;
+                }
+            }
+            if(lineScore > 9)
+            {
+                foreach(RaycastHit hit in hits)
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
     }
 }
