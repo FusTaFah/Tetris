b6180b7d087fbb729f68dbff4b1a32995a8893a8
FusTaFah <mfadel_1990@hotmail.com>
2017-01-23 18:02:53 +0000

    adds tetrimino verifier

---
+++ Assets/Scripts/TetriminoVerifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetriminoVerifier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SignalTetriminoVerifier()
+    {
+        for(int i = 0; i < )
+    }
+}
