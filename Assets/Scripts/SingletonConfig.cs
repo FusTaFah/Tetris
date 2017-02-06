6e0283509a4f0e58cccb071f172a4c33dcffd80c
FusTaFah <mfadel_1990@hotmail.com>
2017-02-06 17:15:51 +0000

    adds lighting effects [DOES NOT WORK] and singleton for main menu

---
+++ Assets/Scripts/SingletonConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SingletonConfig : MonoBehaviour {
+
+    int level;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+        level = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        if(SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            level = (int)GameObject.Find("Slider").GetComponent<Slider>().value;
+            SceneManager.LoadScene("demo");
+        }
+        
+    }
+}
