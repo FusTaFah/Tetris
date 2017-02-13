a016e42c20e00c1ecb2741d8e78fc0e7cf584a2f
FusTaFah <mfadel_1990@hotmail.com>
2017-02-13 18:43:49 +0000

    adds nyetrimino creation room

---
+++ Assets/Scripts/BlankTetriminoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlankTetriminoBehaviour : MonoBehaviour {
+
+    bool m_selected;
+
+	// Use this for initialization
+	void Start () {
+        m_selected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelection()
+    {
+        m_selected = !m_selected;
+        if (m_selected)
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(
+                gameObject.GetComponent<Renderer>().material.color.r,
+                gameObject.GetComponent<Renderer>().material.color.g,
+                gameObject.GetComponent<Renderer>().material.color.b,
+                1.0f
+            );
+        }else
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(
+                gameObject.GetComponent<Renderer>().material.color.r,
+                gameObject.GetComponent<Renderer>().material.color.g,
+                gameObject.GetComponent<Renderer>().material.color.b,
+                0.5f
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/CreationScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 originPoint = new Vector3(-2.5f, 2.0f, 0.0f);
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                Instantiate(Resources.Load("Prefabs/BlankTetrimino"), originPoint + new Vector3(j, -i, 0), Quaternion.identity);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //obtain the current mouse position as pixels from the bottom left of the screen
+        Vector3 mousePos = Input.mousePosition;
+        //create a temporary variable to store the camera position
+        Vector3 newPos = gameObject.transform.position;
+        //store the mouse position in a temporary variable
+        Vector3 cameraToWorld = mousePos;
+        //translate this position such that it is on the near clipping plane
+        cameraToWorld.z = Camera.main.nearClipPlane;
+        //obtain the world point of the near clipping plane
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(cameraToWorld);
+        //use the world point of the mouse and the camera's position to get a direction
+        //towards the world
+        Vector3 cameraToWorldDirection = (worldPoint - gameObject.transform.position).normalized;
+        //use this position to initialise a ray
+
+        Ray x = new Ray(gameObject.transform.position, cameraToWorldDirection);
+        //prepare a raycast towards where the mouse is pointing
+        RaycastHit rch;
+        Physics.Raycast(x, out rch);
+
+        if (rch.collider != null)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if(rch.collider.gameObject.tag == "BlankTetriminoPiece")
+                {
+                    rch.collider.gameObject.GetComponent<BlankTetriminoBehaviour>().OnSelection();
+                }
+            }
+        }
+
+        
+    }
+}
